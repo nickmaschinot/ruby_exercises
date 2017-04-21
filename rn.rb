145beb7300ffb025bc9eac7334c0573786bd97fe
nickmaschinot <nicholas.maschinot@gmail.com>
2017-04-21 15:43:07 -0600

    File update

---
+++ rn.rb
@@ -0,0 +1,24 @@
+puts "give me a number and I will give it back in Roman Numerals (Using up to 4-digits)"
+
+def romanizer number
+  m = number/1000.to_i
+  number = number - m * 1000
+  d = number/500.to_i
+  number = number - d * 500
+  c = number/100.to_i
+  number = number - c * 100
+  l = number/50.to_i
+  number = number - l * 50
+  x = number/10.to_i
+  number = number - x * 10
+  v = number/5.to_i
+  number = number - v * 5
+  i = number.to_i
+
+    puts "M" * m + "D" * d + "C" * c + "L" * l + "X" * x + "V" * v + "I" * i
+
+end
+
+number = gets.chomp.to_i
+
+romanizer number
