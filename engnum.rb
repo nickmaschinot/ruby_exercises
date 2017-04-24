bbc3d8ebcce7438c3a8ee58c15c9e775e4f59d51
nickmaschinot <nicholas.maschinot@gmail.com>
2017-04-23 18:59:03 -0600

    File update

--- engnum.rb
+++ engnum.rb
@@ -50,49 +50,53 @@ def english_number number
      tn = tn - th * 100
      tt = tn/10
      tn = tn - tt * 10
+
   b = number/1000000000
     if b >= 1
       btrig = 4
       if b >= 100
         bhrig = 1
       end
-      end
+    end
   number = number - b * 1000000000
      bn = b
      bh = bn/100
      bn = bn - bh * 100
      bt = bn/10
      bn = bn - bt * 10
+
   m = number/1000000
     if m >= 1
       mtrig = 3
       if m >= 100
         mhrig = 1
       end
-      end
+    end
   number = number - m * 1000000
      mn = m
      mh = mn/100
      mn = mn - mh * 100
      mt = mn/10
      mn = mn - mt * 10
+
   d = number/1000
     if d >= 1
       dtrig = 2
       if d >= 100
         dhrig = 1
       end
-      end
+    end
   number = number - d * 1000
      dn = d
      dh = dn/100
      dn = dn - dh * 100
      dt = dn/10
      dn = dn - dt * 10
+     
   h = number/100
-     if h >= 1
-       htrig = 1
-     end
+    if h >= 1
+      htrig = 1
+    end
   number = number - h * 100
   x = number/10
   number = number - x * 10
