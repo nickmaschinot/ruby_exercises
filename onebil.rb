975ddcaebb44cd72ab2ed2dcc9a66d3bac9f3ef4
nickmaschinot <nicholas.maschinot@gmail.com>
2017-04-25 16:23:03 -0600

    File update

---
+++ onebil.rb
@@ -0,0 +1,21 @@
+
+def how_long_til(year, month, day, hour, minute, second, til)
+
+start = Time.local(year, month, day, hour, minute, second)
+
+onebil = start + til
+
+end
+
+#puts how_long_til(1992, 7, 3, 15, 43, 12, 1000000000)
+
+def happy_birthday(year, month, day)
+  b_day = Time.local(year, month, day)
+  today = Time.now
+  while today - b_day > 0
+    puts "You had a birthday on #{b_day}"
+    b_day += 31558000
+  end
+end
+
+happy_birthday(1992, 7, 3)
