68b2f94a470bcbbd1abf2ff0225a3a2c1cc97aeb
nickmaschinot <nicholas.maschinot@gmail.com>
2017-04-26 14:33:11 -0600

    File update

---
+++ bdaydb.rb
@@ -0,0 +1,71 @@
+
+
+hash = {
+  "Christopher Alexander: " => "Oct 4, 1936",
+  "Christopher Lambert: "   => "Mar 29, 1957",
+  "Christopher Lee: "       => "May 27, 1922",
+  "Christopher Lloyd: "     => "Oct 22, 1938",
+  "Christopher Pine: "      => "Aug 3, 1976",
+  "Christopher Plummer: "   => "Dec 13, 1927",
+  "Christopher Walken: "    => "Mar 31, 1938",
+}
+
+hash.each { |key, date| puts key date }
+
+months = ["", "jan", "feb", "mar", "apr", "may", "jun", "jul",
+          "aug", "sept", "oct", "nov", "dec"]
+
+
+
+
+def b_day_search hash, key
+  months = ["", "jan", "feb", "mar", "apr", "may", "jun", "jul",
+            "aug", "sept", "oct", "nov", "dec"]
+  hash.each do |key, b_day|
+    puts hash[key]
+    b_day = hash[key]
+    b_day = b_day.downcase
+    b_day = b_day.split(/\W+/)
+
+    puts b_day
+    mon = months.index("#{b_day[0]}")
+    day = b_day[1].to_i
+    year = b_day[2].to_i
+    mon = mon.to_i
+    dob = Time.local(year, mon, day)
+    present = Time.new
+    present = present.to_s
+    present = present.split(/\W+/)
+    pyear = present[0].to_i
+    pmon = present[1].to_i
+    pday = present[2].to_i
+    age_day = pday - day
+    age_mon = pmon - mon
+      if age_day < 0
+        age_mon -= 1
+        age_day = 31 + age_day
+      end
+    age_year = pyear - year
+      if age_mon < 0
+        age_year -= 1
+        age_mon = 12 + age_mon
+      end
+
+    b_day_year = year + age_year + 1
+
+    next_b_day = "#{mon}/#{day}/#{b_day_year}"
+    how_old = "This person is #{age_year}-years, #{age_mon}-months, and #{age_day}-days old."
+    puts how_old
+    puts "Their next birthday is on #{next_b_day}"
+    # add this new info to the hash
+
+    puts dob
+
+    puts mon
+
+  end
+  puts hash["Christopher Alexander: "]
+  # make it so only the requested individual is displayed
+end
+
+b_day_search hash
