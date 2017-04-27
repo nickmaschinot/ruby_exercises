d88ba1b394ecfbac22b68035ec8e973d0fea36ee
nickmaschinot <nicholas.maschinot@gmail.com>
2017-04-27 17:38:04 -0600

    File update

--- orange_tree.rb
+++ orange_tree.rb
@@ -7,6 +7,7 @@ class OrangeTree
     @health = 100
     @age = 0
     @sick = 0
+    $counter=1
   end
 
   def water
@@ -16,10 +17,10 @@ class OrangeTree
   end
 
   def count_the_oranges
-    if @oranges == 0
+    if @oranges < 1
       puts "no oranges to see this year :("
     else
-      puts "there are #{@oranges} this year"
+      puts "there are #{@oranges} oranges this year"
     end
     one_year_passes
   end
@@ -31,7 +32,7 @@ class OrangeTree
   end
 
   def measure_tree
-    puts "The tree is #{@height}ft tall"
+    puts "#{@name} is #{@height}ft tall"
     puts "and #{@width}ft wide now"
     one_year_passes
   end
@@ -44,17 +45,20 @@ class OrangeTree
     @health_factor = @health - @age
 
     if @sick == 1 && rand(1..100) < 30
-      puts "#{@name} is dead!"
-      counter = 0
+      puts "#{@name} is dead from its sickness!"
+      puts "#{@name} was #{@age} years old"
+      $counter = 0
       return
     elsif @sick == 1 && rand(1..100) > 30
       puts "#{@name} has recovered from it's illness!"
       @sick = 0
+      @health += 5
     end
 
     if @health - @age < 1
       puts "#{@name} died suddenly :("
-      counter = 0
+      puts "#{@name} was #{@age} years old"
+      $counter = 0
       return
     elsif rand(1..@health_factor) == 1
       puts "It looks like #{@name} is sick"
@@ -72,34 +76,46 @@ class OrangeTree
       end
     end
 
+    @height += (4 * (1.0/(1.0 + @age - 1.5)))
+    @height = @height.round(2)
 
-
-    @height += (@age * (4/@age))
-
-    @width += (@age * (3/@age))
+    @width += (3 * (1.0/(1.0 + @age - 1.5)))
+    @width = @width.round(2)
 
     puts "#{@name} is now #{@age} years old."
 
   end
 end
 
-
-counter = 1
-
-ot = OrangeTree.new "Charlie"
-puts "Here is your new orange tree"
-  while counter > 0
-    puts "Would you like to 'water', 'measure', 'count oranges', or 'eat an orange'?"
-    input = gets.chomp.downcase
-    if input == "water"
-      ot.water
-    elsif input == "measure"
-      ot.measure_tree
-    elsif input == "count oranges"
-      ot.count_the_oranges
-    elsif input == "eat an orange"
-      ot.eat_an_orange
-    else
-      puts "please use one of the four listed inputs"
+puts "plant an orange tree? yes or no"
+#ruby orange_tree.rb
+x = true
+while x == true
+  response = gets.chomp.downcase
+  if response == "yes"
+    puts "what is the new tree's name?"
+    name = gets.chomp
+    ot = OrangeTree.new name
+    puts "Here is your new orange tree"
+    while $counter > 0
+      puts "Would you like to 'water', 'measure', 'count oranges', or 'eat an orange'?"
+      input = gets.chomp.downcase
+      if input == "water"
+        ot.water
+      elsif input == "measure"
+        ot.measure_tree
+      elsif input == "count oranges"
+        ot.count_the_oranges
+      elsif input == "eat an orange"
+        ot.eat_an_orange
+      else
+        puts "please use one of the four listed inputs"
+      end
     end
+  puts "plant another tree? yes or no?"
+  elsif response == "no"
+    x = false 
+  else
+    puts "please answer yes or no"
   end
+end
