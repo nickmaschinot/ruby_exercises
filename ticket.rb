305b9cbc1f72f462a5cea1a966766365e15216e0
nickmaschinot <nicholas.maschinot@gmail.com>
2017-06-23 21:12:27 -0600

    working with classes

--- ticket.rb
+++ ticket.rb
@@ -1,31 +1,43 @@
-ticket = Object.new
 
-def date
-  "01/02/03"
-end
-def venue
-  "Town Hall"
-end
-def event
-  "Author's Reading"
-end
-def performer
-  "Mark Twain"
-end
-def seat
-  "Second Balcony, row 3, seat 12"
-end
-def price
-  5.50
+class Event
+  attr_accessor :performer
 end
 
-print "This ticket is for: "
-print event + ", at "
-print venue + ", on "
-puts date + "."
-print "The performer is "
-puts performer + "."
-print "Your seat is "
-print seat + ", "
-print "and it costs $"
-puts "%.2f." % price + "."
+class Ticket < Event
+  VENUES = ["Convention Center", "Fairgrounds", "Town Hall"]
+  attr_reader :venue, :date
+  attr_accessor :price
+  def initialize(venue, date)
+    if VENUES.include?(venue)
+      @venue = venue
+    else
+      raise ArgumentError, "Unknown venue #{venue}"
+    end
+    @date = date
+  end
+end
+
+def Ticket.most_expensive(*tickets)
+  tickets.max_by(&:price)
+end
+
+th = Ticket.new("Town Hall", "11/13/14")
+cc = Ticket.new("Convention Center", "12/13/14")
+fg = Ticket.new("Fairgrounds", "12/13/14")
+
+
+th.price = 63.00
+cc.price = 45.25
+fg.price = 100.00
+highest = Ticket.most_expensive(th,cc,fg)
+puts "The most expensive ticket is for #{highest.venue}."
+
+th.performer = "The Boss"
+
+puts "First ticket is for a #{th.venue} event on #{th.date}, and it costs
+      $#{"%.2f" % th.price}. #{th.performer} will be performing."
+th.price = (72.50)
+puts "Oops, it just went up and now costs $#{"%.2f" % th.price}."
+puts "The second ticket is for a #{cc.venue} event on #{cc.date}."
+
+puts Ticket::VENUES
