37ebe37dca15ada7622c32cab6a0ba20d6159bab
nickmaschinot <nicholas.maschinot@gmail.com>
2017-06-25 13:57:18 -0600

    replicating class methods: times, each, and map

---
+++ cmethods.rb
@@ -0,0 +1,32 @@
+class Integer
+  def my_times
+    c = 0
+    until c == self
+      yield(c)
+      c += 1
+    end
+    self
+  end
+end
+
+class Array
+  def my_each
+    c = 0
+    until c == size
+      yield(self[c])
+      c += 1
+    end
+    self
+  end
+  def my_map
+    acc = []
+    my_each {|e| acc << yield(e)}
+    acc
+  end
+end
+
+
+5.my_times {|i| puts "iteration #{i}"}
+array = [1,4,3,4,7]
+array.my_each {|e| puts "my_each was handed #{e}"}
+array.my_map {|e| puts e * 10}
