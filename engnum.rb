7cc572f3928be7a021562d9bc9e6ae1b8bbd8b25
nickmaschinot <nicholas.maschinot@gmail.com>
2017-06-26 14:55:53 -0600

    using modules to improve array utility

--- engnum.rb
+++ engnum.rb
@@ -1,93 +1,5 @@
-
-def english_number (number)
-  ones_place = ["", "one ", "two ", "three ", "four ", "five ",
-                "six ", "seven ", "eight ", "nine "]
-  teens = ["", "eleven ", "twelve", "thirteen ", "fourteen ", "fifteen ", "sixteen ",
-           "seventeen ", "eighteen ", "nineteen "]
-  tens_place = ["", "ten ", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ",
-                "eighty ", "ninety "]
-  higher_place = ["", "hundred ", "thousand ", "million ", "billion ", "trillion "]
-
-  sign = ""
-
-  if number < 0
-    sign = "negative-"
-  elsif number == 0
-    puts "zero"
-  end
-
-  number = number.abs
-
-  ttrig = 0; thrig = 0; btrig = 0; bhrig = 0; mtrig = 0; mhrig = 0; dtrig = 0
-  dhrig = 0; htrig = 0
-
-  t = number/1000000000000
-    if t >= 1
-      ttrig = 5
-      if t >= 100
-        thrig = 1
-      end
-    end
-  number = number - t * 1000000000000
-    tn = t; th = tn/100; tn = tn - th * 100; tt = tn/10; tn = tn - tt * 10
-
-  b = number/1000000000
-    if b >= 1
-      btrig = 4
-      if b >= 100
-        bhrig = 1
-      end
-    end
-  number = number - b * 1000000000
-    bn = b; bh = bn/100; bn = bn - bh * 100; bt = bn/10; bn = bn - bt * 10
-
-  m = number/1000000
-    if m >= 1
-      mtrig = 3
-      if m >= 100
-        mhrig = 1
-      end
-    end
-  number = number - m * 1000000
-    mn = m; mh = mn/100; mn = mn - mh * 100; mt = mn/10; mn = mn - mt * 10
-
-  d = number/1000
-    if d >= 1
-      dtrig = 2
-      if d >= 100
-        dhrig = 1
-      end
-    end
-  number = number - d * 1000
-    dn = d; dh = dn/100; dn = dn - dh * 100; dt = dn/10; dn = dn - dt * 10
-
-  h = number/100
-    if h >= 1
-      htrig = 1
-    end
-  number = number - h * 100
-  x = number/10
-  number = number - x * 10
-  n = number
-
-  answer = sign + ones_place[th] + higher_place[thrig] + tens_place[tt] + ones_place[tn] +
-  higher_place[ttrig] + ones_place[bh] + higher_place[bhrig] + tens_place[bt] +
-  ones_place[bn] + higher_place[btrig] + ones_place[mh] + higher_place[mhrig] +
-  tens_place[mt] + ones_place [mn] + higher_place[mtrig] + ones_place[dh] +
-  higher_place[dhrig] + tens_place[dt] + ones_place [dn] + higher_place[dtrig] +
-  ones_place[h] + higher_place[htrig] + tens_place[x] + ones_place[n]
-
-    answer.gsub! "ten nine", "nineteen"; answer.gsub! "ten eight", "eighteen"
-    answer.gsub! "ten seven", "seventeen"; answer.gsub! "ten six", "sixteen"
-    answer.gsub! "ten five", "fifteen"; answer.gsub! "ten four", "fourteen"
-    answer.gsub! "ten three", "thirteen"; answer.gsub! "ten two", "twelve"
-    answer.gsub! "ten one", "eleven"; answer.gsub! " ", "-"
-    answer = answer.chop
-
-  puts "Here is your number:"
-  puts answer
-
-end
+require_relative "english_number"
+include EngNum
 
 puts "give me a number and I'll give it back using words (Using up to 15 digits)"
 while true
@@ -99,7 +11,7 @@ while true
     puts "Please only input integers"
   else
     number = number.to_i
-    english_number (number)
+    p "Here is your number: #{english_number(number)}"
   end
   puts "try again? or QUIT?"
 end
--- hashes.rb
+++ hashes.rb
@@ -1,3 +1,5 @@
+require_relative "english_number"
+
 hash = {
   red: "ruby",
   white: "diamond",
@@ -19,47 +21,41 @@ end
 class Array
   include ArraySup
 end
-
+class Integer
+  include EngNum
+end
 
 arr= Array.new(2,"a")
 n = 0
 arr2 = Array.new(4) { n += 1; n * 8 }
 puts arr
 puts arr2
-
+p arr2.sum_array(arr2)
+p arr.size
 hidden_num = Array.new(8,3)
 hidden_num << 5
 hidden_num.shuffle!
 p hidden_num
 
 
+#only works with the classic two guesses to find one heavy ball out of nine puzzle
 def num_guess(array)
   message = "hidden number is in position"
+  i = 0
   if array.sum_array(array[0..2]) == array.sum_array(array[3..5])
-    if array[6] == array[7]
-      p "#{message} 8"
-    elsif array[6] > array[7]
-      p "#{message} 6"
-    else
-      p "#{message} 7"
+    if array[6] == array[7]; i = 8
+    elsif array[6] > array[7]; i = 6; else; i = 7
     end
   elsif array.sum_array(array[0..2]) < array.sum_array(array[3..5])
-    if array[3] == array[4]
-      p "#{message} 5"
-    elsif array[3] > array[4]
-      p "#{message} 3"
-    else
-      p "#{message} 4"
+    if array[3] == array[4]; i = 5
+    elsif array[3] > array[4]; i = 3; else i = 4
     end
   else
-    if array[0] == array[1]
-      p "#{message} 2"
-    elsif array[0] > array[1]
-      p "#{message} 0"
-    else
-      p "#{message} 1"
+    if array[0] == array[1]; i = 2
+    elsif array[0] < array[1]; i = 1
     end
   end
+  p "#{message} #{i.english_number(i)}"
 end
 
 num_guess(hidden_num)
