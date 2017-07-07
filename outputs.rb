80c243c923598c6ebbffead2ab4b3d41ee5f2324
nickmaschinot <nicholas.maschinot@gmail.com>
2017-07-07 13:26:17 -0600

    working with file operations

---
+++ outputs.rb
@@ -0,0 +1,14 @@
+record = File.open("temp.out", "w")
+old_stdout = $stdout
+$stdout = record
+$stderr = $stdout
+f = File.new("c2fout.rb")
+puts "This is a record"
+3.times do
+  puts f.gets
+end
+f.rewind
+p f.readlines
+f.rewind
+f.each {|line| puts "Next line: #{line}" }
+z = 10/0
