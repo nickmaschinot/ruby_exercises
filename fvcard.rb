2c9097cb68d0c1e1e595d57db18120034df32075
nickmaschinot <nicholas.maschinot@gmail.com>
2017-07-11 11:42:08 -0600

    starting 5-card draw

---
+++ fvcard.rb
@@ -0,0 +1,102 @@
+require_relative "cards"
+require_relative "game_modules"
+
+class Game < Deck
+  include FiveCardDraw
+  attr_reader :title
+  def initialize
+    super
+    @title = ""
+  end
+end
+
+class Player < Game
+  attr_reader :name
+  attr_accessor :money
+  def initialize(name, money)
+    @name = name
+    @money = money
+  end
+end
+
+
+p "Let's play some Poker"
+p "What's your name?"
+name = gets.chomp
+p "How much do you want to play with?"
+bnkroll = gets.chomp.to_i
+plyr = Player.new(name, bnkroll)
+p "Alright #{plyr.name}, let's play"
+while true
+  p "place your bet or type QUIT"
+  while true
+    bet = gets.chomp.downcase
+    if bet == "quit"
+      puts "Goodbye"
+      return
+    elsif bet.count("a-z") > 0
+      puts "Please only input integers or quit"
+    else
+      bet = bet.to_i
+      if bnkroll - bet < 0
+        p "You dont have enough money to place that bet"
+        p "You have $#{plyr.money} remaining"
+        p "Place a smaller bet:"
+      else
+        break
+      end
+    end
+  end
+  p "--------------------------------------"
+  deck = Game.new
+  deck.shuffle_deck
+  player1 = deck.hand_new(5)
+  player2 = deck.hand_new(5)
+  remainder = deck.hand_new(42)
+  deck.display(player1, "your")
+  p "--------------------------------------"
+  p "what cards do you want to get rid of before you draw?"
+  discard = []
+  while true
+    p "type '1', '2', '3', '4', '5' to discard the cards you dont want."
+    p "and 'draw' to replace them."
+    input = gets.chomp.downcase
+    if input == "draw"
+      discard
+      break
+    elsif input.count("a-z") > 0
+      puts "Please only input integers or draw"
+    elsif input.count("67890") > 0
+      puts "only numbers 1 to 5 will discard a card"
+    else
+      input = input.scan(/\w/)
+      input.each do |number|
+        number = number.to_i - 1
+        if discard.include?(number)
+          discard.slice!(number)
+          p "no longer discarding the #{player1[number][0]} of #{player1[number][1]}s"
+        else
+          discard << number
+          p "discarding the #{player1[number][0]} of #{player1[number][1]}s"
+        end
+      end
+    end
+  end
+  discard.each do |y|
+    player1 = player1.map{|x|x == player1[y] ? remainder[0] : x}
+    p "You drew the #{remainder[0][0]} of #{remainder[0][1]}s"
+    remainder.slice!(0)
+  end
+  deck.display(player1, "your")
+  p "--------------------------------------"
+  plyr.money += deck.score_hand(player1, bet)
+  p "--------------------------------------"
+  p "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  p "--------------------------------------"
+  if plyr.money == 0
+    puts "Game over"
+    break
+  end
+  p "you have $#{plyr.money}"
+  p "play again? or QUIT?"
+end
--- game_modules.rb
+++ game_modules.rb
@@ -187,3 +187,121 @@ module VideoPoker
     hand.each_cons(2).all? {|a, b| b == a + 1 }
   end
 end
+
+module FiveCardDraw
+  TYPES = ["Royal-Flush", "Straight-Flush", "Four-of-a-Kind", "Full House", "Flush",
+            "Straight", "Three-of-a-kind", "Two-Pair", "Pair", "High-Card"]
+  PRIZES = [250, 50, 25, 9, 6, 4, 3, 2, 1]
+  def hand_type(hand)
+    by_suit = []
+    by_value = []
+    hand.each do |k|
+      by_suit << k[1]
+    end
+    by_suit.sort!
+    hand.each do |k|
+      by_value << k[2]
+    end
+    by_suit.sort!
+    frequency = by_value.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    frequency.values.sort.reverse!
+
+    if all_equal?(by_suit)
+      if straight?(by_value)
+        if hand_score(by_value) == 60
+          TYPES[0]
+        else
+          TYPES[1]
+        end
+      else
+        TYPES[4]
+      end
+    elsif straight?(by_value)
+      TYPES[5]
+    elsif frequency.values.sort.last == 4
+      TYPES[2]
+    elsif frequency.values.sort.last == 3
+      if frequency.count == 2
+        TYPES[3]
+      else
+        TYPES[6]
+      end
+    elsif frequency.values.sort.last == 2
+      if frequency.count == 3
+        TYPES[7]
+      else
+        if frequency.key(frequency.values.sort.last) >= 11
+          TYPES[8]
+        else
+          TYPES[9]
+        end
+      end
+    else
+      TYPES[9]
+    end
+  end
+
+  def winner?(title)
+    return true unless title == TYPES[9]
+  end
+
+  def score_hand(hand, bet)
+    type = hand_type(hand)
+    payout = payout(bet, type)
+    if winner?(type)
+      p "You win with a #{type}"
+      p "your payout is $#{payout}"
+      bet = payout - bet
+    else
+      p "you lose"
+      bet = -bet
+    end
+  end
+
+  def payout(bet, title)
+    payout = ""
+    case title
+    when TYPES[0]
+      payout = PRIZES[0]
+    when TYPES[1]
+      payout = PRIZES[1]
+    when TYPES[2]
+      payout = PRIZES[2]
+    when TYPES[3]
+      payout = PRIZES[3]
+    when TYPES[4]
+      payout = PRIZES[5]
+    when TYPES[6]
+      payout = PRIZES[6]
+    when TYPES[7]
+      payout = PRIZES[7]
+    when TYPES[8]
+      payout = PRIZES[8]
+    end
+    payout * bet
+  end
+  def hand_score(values)
+    @score = 0
+    values.count.times do |x|
+        @score += values[x]
+    end
+    @score
+  end
+  def display(hand, name)
+    x = 0
+    y = 1
+    p "#{name} hand: "
+    hand.count.times do |x|
+      print "#{y}: #{hand[x][0]} of #{hand[x][1]}s | \n"
+      x += 1
+      y += 1
+    end
+  end
+  def all_equal?(hand)
+      hand.uniq.size <= 1
+  end
+  def straight?(hand)
+    return false unless hand.size == 5
+    hand.each_cons(2).all? {|a, b| b == a + 1 }
+  end
+end
